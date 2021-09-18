ac640cc5cdb7876d6194a801ee3feff7ff6ff071
Exvigilare Gemini <dubois.adelin@gmail.com>
2021-09-18 22:09:17 +0200

    Fix sorting : now is sorting date format column

--- src/lib/functions/sort.js
+++ src/lib/functions/sort.js
@@ -1,13 +1,37 @@
+function compareArrays(arr1, arr2, index) {
+  return arr1[index].cellValue > arr2[index].cellValue ? true : false;
+}
+
+function reverseDateRepresentation(arr, index) {
+  let parts = arr[index].cellValue.split("/");
+  return `${parts[2]}-${parts[1]}-${parts[0]}`;
+}
+
+function compareArraysOfDateFormat(arr1, arr2, index) {
+  const arrDateFormat1 = reverseDateRepresentation(arr1, index);
+  const arrDateFormat2 = reverseDateRepresentation(arr2, index);
+  return arrDateFormat1 > arrDateFormat2 ? true : false;
+}
+
+const regexIsDateFormat = /[0-9]{2}[/][0-9]{2}[/][0-9]{4}/;
+
 export function sortingColumn(arrayToSort, sortingOrder, categoryToCompare) {
   if (sortingOrder !== 0 || categoryToCompare !== "") {
-    arrayToSort.sort((a, b) => {
-      const indexOfCorrespondingCategory = a.findIndex(
+    arrayToSort.sort((arr1, arr2) => {
+      const indexOfCorrespondingCategory = arr1.findIndex(
         (el) => el.category === categoryToCompare
       );
+      console.log("Issue 1");
+      // If in date format
       if (
-        a[indexOfCorrespondingCategory].cellValue >
-        b[indexOfCorrespondingCategory].cellValue
+        regexIsDateFormat.test(arr1[indexOfCorrespondingCategory].cellValue)
       ) {
+        if (
+          compareArraysOfDateFormat(arr1, arr2, indexOfCorrespondingCategory)
+        ) {
+          return sortingOrder;
+        }
+      } else if (compareArrays(arr1, arr2, indexOfCorrespondingCategory)) {
         return sortingOrder;
       }
       return -sortingOrder;
