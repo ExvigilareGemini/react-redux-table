504a01d55036163143eb802a2e92be341e7194bb
Exvigilare Gemini <dubois.adelin@gmail.com>
2021-09-28 19:41:48 +0200

    Fix sortingColumn : handle sortingOrder & categoryToCompare if they are undefined

--- dist/functions/sort.js
+++ dist/functions/sort.js
@@ -17,14 +17,13 @@ require("core-js/modules/es.array.sort.js");
  * @param {Object[]} arr1 - First array
  * @param {Object[]} arr2 - Second array
  * @param {number} index - The place of the object to compare inside both arrays
- * @returns {bool} - True if the value in first array is bagger than in the second, false if not
+ * @returns {bool} - True if the value in first array is bigger than in the second, false if not
  */
 function compareArrays(arr1, arr2, index) {
-  console.log(arr1);
   return arr1[index].cellValue > arr2[index].cellValue ? true : false;
 }
 /**
- * Invert the string date format from dd/mm/yyyy to yyyy/mm/dd and vice versa 
+ * Invert the string date format from dd/mm/yyyy to yyyy/mm/dd and vice versa
  * @category Functions
  * @param {string} date - The date to invert
  * @returns {string} - The invertes date
@@ -62,21 +61,21 @@ const regexIsDateFormat = /[0-9]{2}[/][0-9]{2}[/][0-9]{4}/;
  */
 
 function sortingColumn(arrayToSort, sortingOrder, categoryToCompare) {
-  if (sortingOrder !== 0 || categoryToCompare !== "") {
-    arrayToSort.sort((arr1, arr2) => {
-      const indexOfCorrespondingCategory = arr1.findIndex(el => el.category === categoryToCompare); // If in date format
-
-      if (regexIsDateFormat.test(arr1[indexOfCorrespondingCategory].cellValue)) {
-        if (compareArraysOfDateFormat(arr1, arr2, indexOfCorrespondingCategory)) {
-          return sortingOrder;
-        }
-      } else if (compareArrays(arr1, arr2, indexOfCorrespondingCategory)) {
-        return sortingOrder;
-      }
-
-      return -sortingOrder;
-    });
+  if (sortingOrder === 0 || sortingOrder === undefined || categoryToCompare === "" || categoryToCompare === undefined) {
+    return arrayToSort;
   }
 
-  return arrayToSort;
+  arrayToSort.sort((arr1, arr2) => {
+    const indexOfCorrespondingCategory = arr1.findIndex(el => el.category === categoryToCompare); // If in date format
+
+    if (regexIsDateFormat.test(arr1[indexOfCorrespondingCategory].cellValue)) {
+      if (compareArraysOfDateFormat(arr1, arr2, indexOfCorrespondingCategory)) {
+        return sortingOrder;
+      }
+    } else if (compareArrays(arr1, arr2, indexOfCorrespondingCategory)) {
+      return sortingOrder;
+    }
+
+    return -sortingOrder;
+  });
 }
