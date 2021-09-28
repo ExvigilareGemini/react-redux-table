b9ff65f9b26d15ed43b398ac3c3eb27556cd18c5
Exvigilare Gemini <dubois.adelin@gmail.com>
2021-09-28 21:27:23 +0200

    Refactor : set the redux state in a rrtable object

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
-import Table from "./lib/components/Table"
+import Table from "./lib/components/Table";
 import { rrtable } from "./lib/redux/reducer/rrtable-reducer";
 import { headersArray, rowsContent, entriesSelector } from "./datas/mock";
 
 const store = configureStore({
-  reducer: rrtable,
+  reducer: { rrtable: rrtable },
 });
 
 const App = () => {
