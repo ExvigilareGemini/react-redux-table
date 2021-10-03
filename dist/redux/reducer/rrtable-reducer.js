329fefd3caefe9fc2a08899a151de8c4c099589c
Exvigilare Gemini <dubois.adelin@gmail.com>
2021-10-04 00:10:31 +0200

    Build npm : v0.1.22

--- dist/redux/reducer/rrtable-reducer.js
+++ dist/redux/reducer/rrtable-reducer.js
@@ -3,7 +3,7 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.rrtableReducer = rrtableReducer;
+exports.default = rrtableReducer;
 
 var _rrtableTypes = require("../constants/rrtable-types");
 
